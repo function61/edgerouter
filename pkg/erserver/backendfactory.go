171a851061dd42e002ec11f3d5bc7ec1988e1b13
Joonas Loppi <joonas@joonas.fi>
2020-03-09 12:05:59 +0200

    backendfactory: add cache so we won't make new instances of backends all the time
    new instance of peersetbackend made new instance of http.Transport which ended
    up with loads of half-open TCP conns

--- pkg/erserver/backendfactory.go
+++ pkg/erserver/backendfactory.go
@@ -1,6 +1,8 @@
 package erserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/function61/edgerouter/pkg/erbackend/authv0backend"
 	"github.com/function61/edgerouter/pkg/erbackend/lambdabackend"
@@ -10,12 +12,37 @@ import (
 	"net/http"
 )
 
+var bendCache = newBackendCache()
+
 // TODO: make "fem" parameteter unnecessary
 func makeBackend(
 	appId string,
 	backendConf erconfig.Backend,
 	fem *frontendMatchers,
 ) (http.Handler, error) {
+	configDigest, err := json.Marshal(backendConf)
+	if err != nil {
+		return nil, err
+	}
+
+	cached := bendCache.Find(appId, configDigest)
+	if cached == nil {
+		backend, err := makeBackendInternal(appId, backendConf, fem)
+		if err != nil {
+			return nil, err
+		}
+
+		cached = &cacheEntry{
+			backend:      backend,
+			configDigest: configDigest,
+		}
+		bendCache.perAppId[appId] = cached
+	}
+
+	return cached.backend, nil
+}
+
+func makeBackendInternal(appId string, backendConf erconfig.Backend, fem *frontendMatchers) (http.Handler, error) {
 	switch backendConf.Kind {
 	case erconfig.BackendKindS3StaticWebsite:
 		return statics3websitebackend.New(appId, *backendConf.S3StaticWebsiteOpts), nil
@@ -26,7 +53,7 @@ func makeBackend(
 	case erconfig.BackendKindEdgerouterAdmin:
 		return newAdminBackend(fem), nil
 	case erconfig.BackendKindAuthV0:
-		authorizedBackend, err := makeBackend(
+		authorizedBackend, err := makeBackendInternal(
 			appId,
 			backendConf.AuthV0Opts.AuthorizedBackend,
 			fem)
@@ -38,4 +65,36 @@ func makeBackend(
 	default:
 		return nil, fmt.Errorf("unsupported backend kind: %s", backendConf.Kind)
 	}
+}
+
+// we need this because if we'd make new instances all the time, peersetbackend f.ex. makes
+// new http.Transport instance each time, so this would end up with loads of half-open TCP
+// connections since the connection cache is per http.Transport
+// NOTE: no need for locking, because makeBackend() is not called concurrently
+type backendCache struct {
+	perAppId map[string]*cacheEntry
+}
+
+type cacheEntry struct {
+	backend      http.Handler
+	configDigest []byte
+}
+
+func newBackendCache() *backendCache {
+	return &backendCache{
+		perAppId: map[string]*cacheEntry{},
+	}
+}
+
+func (b *backendCache) Find(appId string, configDigest []byte) *cacheEntry {
+	cached, found := bendCache.perAppId[appId]
+	if !found {
+		return nil
+	}
+
+	if !bytes.Equal(configDigest, cached.configDigest) {
+		return nil
+	}
+
+	return cached
 }

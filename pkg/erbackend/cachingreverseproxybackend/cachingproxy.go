40885a6b099d3a5da98f2441be854cbe270ddf7b
Joonas Loppi <joonas@joonas.fi>
2021-02-06 14:09:08 +0200

    cachingreverseproxy: ignore query string with regards to caching the origin

--- pkg/erbackend/cachingreverseproxybackend/cachingproxy.go
+++ pkg/erbackend/cachingreverseproxybackend/cachingproxy.go
@@ -20,8 +20,7 @@ func New(appId string, opts erconfig.BackendOptsCachingReverseProxy) (http.Handl
 		return nil, fmt.Errorf("cachingreverseproxy: %w", err)
 	}
 
-	// TODO: maybe make configurable
-	// there's no abstraction for getting system-level cache in Go
+	// there's no abstraction for getting system-level cache dir in Go
 	cacheLocation := filepath.Join("/var/cache/edgerouter", appId)
 
 	if err := os.MkdirAll(cacheLocation, 0700); err != nil {
@@ -36,10 +35,15 @@ func New(appId string, opts erconfig.BackendOptsCachingReverseProxy) (http.Handl
 		Director: func(req *http.Request) {
 			req.URL.Scheme = originUrl.Scheme
 			req.URL.Host = originUrl.Host
+			req.Host = req.URL.Host // needs to be present both in the URL and the request
+
 			// origin's Path is "normally" empty, but can be used to add prefix
 			req.URL.Path = originUrl.Path + req.URL.Path
 
-			req.Host = req.URL.Host
+			// remove query string, because we know we're serving static content and the output does
+			// not vary based on query string. someone malicious could even be trying to flood our
+			// origin with requests knowing varying the query is a cache miss
+			req.URL.RawQuery = ""
 		},
 	}, nil
 }
